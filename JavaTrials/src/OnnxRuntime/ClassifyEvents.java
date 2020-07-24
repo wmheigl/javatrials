f4726871bd27df82acfa480359f492aa2a5c0664
Werner M. Heigl <werner.heigl@nanoseis.com>
2020-07-24 17:05:49 -0600

    Adds a model and data directory.

--- JavaTrials/src/OnnxRuntime/ClassifyEvents.java
+++ JavaTrials/src/OnnxRuntime/ClassifyEvents.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 import ai.onnxruntime.NodeInfo;
@@ -36,9 +37,12 @@ import hdf.hdf5lib.*;
  */
 public class ClassifyEvents {
 
-  private static final String MODEL = "/home/werner/ML_Data/CNN_Model_67.onnx";
+//  private static final String MODEL = "/home/werner/ML_Data/CNN_Model_67.onnx";
+  private static final String MODEL = "/home/werner/ML_Data/CNN_Model_188.onnx";
   private static final String MODEL_OPT = "/home/werner/optimModel.onnx";
-  private static final String DATA_DIR = "/meg2/ben/promax_data_home/MachineLearning_Synth_Field/PSEvents_2mil_750x500/";
+//  private static final String DATA_DIR = "/meg2/ben/promax_data_home/MachineLearning_Synth_Field/PSEvents_2mil_750x500/";
+//  private static final String DATA_DIR = "/meg2/ben/promax_data_home/MachineLearning_Synth_Field/Noise_2mil_750x500/";
+  private static final String DATA_DIR = "/meg2/ben/promax_data_home/MachineLearning_Synth_Field/Noise_2mil_750x500/";
   private static final String DATA_FILE = "FFID-000001_AMP-1.0000_CON-0.0_X-3142050.0_Y-1331668.8_ELEV--2368.1_T-1591734621255_CLASS-0.h5";
 
   /**
@@ -63,8 +67,7 @@ public class ClassifyEvents {
 	OrtSession.SessionOptions opts = new SessionOptions()) {
 //@formatter:on
 
-      // opts.setExecutionMode(ExecutionMode.PARALLEL);
-      // opts.setIntraOpNumThreads(4);
+      opts.setExecutionMode(ExecutionMode.PARALLEL);
       opts.setOptimizationLevel(OptLevel.ALL_OPT);
       opts.setOptimizedModelFilePath(MODEL_OPT);
       // opts.addCUDA(); / need to compile from source with CUDA support
@@ -83,13 +86,27 @@ public class ClassifyEvents {
 	int dim2 = (int) dims[2];
 	int dim3 = (int) dims[3];
 
-	for (Path path : fileList) {
+	// get number of output nodes and setup confusion matrix
+	System.out.println("Outputs: " + session.getOutputInfo().values());
+	List<NodeInfo> outputValues = new ArrayList<NodeInfo>(session.getOutputInfo().values());
+	TensorInfo outputInfo = (TensorInfo) outputValues.get(0).getInfo();
+	int nout = (int) outputInfo.getShape()[1];
+	int[][] confusionMatrix = new int[nout][nout];
+	int correctCount = 0;
+
+	/*
+	 * iterate over list of H5 files and keep track of scores
+	 */
+	ListIterator<Path> fileIterator = fileList.listIterator();
+	while(fileIterator.hasNext()) {
+//	for (Path path : fileList) {
 
 	  /*
-	   *  load a data frame and shape it into a rank-4 tensor
+	   * load a data frame and shape it into a rank-4 tensor
 	   */
-	  
-	  float[][] dataFrame = ClassifyEvents.loadH5(path.toString());
+
+	  float[][] dataFrame = ClassifyEvents.loadH5(fileIterator.next().toString());
+//	  float[][] dataFrame = ClassifyEvents.loadH5(path.toString());
 
 	  // sub-sample to 250 samples
 	  float[][] subSampled = new float[dim1][dim2];
@@ -103,18 +120,10 @@ public class ClassifyEvents {
 	    }
 	  }
 
-	  System.out.println("Tensor.info: " + OnnxTensor.createTensor(env, datum).getInfo());
-
-	  // get number of output nodes and setup confusion matrix
-	  System.out.println("Outputs: " + session.getOutputInfo().values());
-	  List<NodeInfo> outputValues = new ArrayList<NodeInfo>(session.getOutputInfo().values());
-	  TensorInfo outputInfo = (TensorInfo) outputValues.get(0).getInfo();
-	  int nout = (int) outputInfo.getShape()[1];
-	  int[][] confusionMatrix = new int[nout][nout];
-	  int correctCount = 0;
+	  // System.out.println("Tensor.info: " + OnnxTensor.createTensor(env, datum).getInfo());
 
 	  String inputName = session.getInputNames().iterator().next();
-	  System.out.println("Input Name: " + inputName);
+	  // System.out.println("Input Name: " + inputName);
 
 	  try (OnnxTensor test = OnnxTensor.createTensor(env, new float[][][][] { datum })) {
 
@@ -122,17 +131,71 @@ public class ClassifyEvents {
 
 	    // 0: noise, 1: PS Event, 2: S-only
 	    float[][] scores = (float[][]) output.get(0).getValue();
-	    System.out.println("Scores: " + Arrays.toString(scores[0]));
+	    // System.out.println("Scores: " + Arrays.toString(scores[0]));
+
+	    int predLabel = ClassifyEvents.pred(scores[0]);
+
+	    if (predLabel == 0) {
+	      correctCount++;
+	    }
+
+	    confusionMatrix[0][predLabel]++;
+
+	    if (fileIterator.previousIndex() % 200 == 0) {
+	      System.out.println("Progress: " + fileIterator.nextIndex() + " files");
+	      System.out.println("Scores: " + Arrays.toString(scores[0]));
+	    }
+
 	  }
-	  
+
 	} // end for-each (path)
-	
+
+	System.out.println();
+	System.out.println("Final accuracy = " + ((float) correctCount) / fileList.size());
+
+	// confusion matrix (vertical axis: actual category, horizontal axis: predicted category)
+	StringBuilder sb = new StringBuilder();
+	sb.append("Label");
+	for (int i = 0; i < confusionMatrix.length; i++) {
+	  sb.append(String.format("%1$6s", "" + i));
+	}
+	sb.append("\n");
+
+	for (int i = 0; i < confusionMatrix.length; i++) {
+	  sb.append(String.format("%1$6s", "" + i));
+	  for (int j = 0; j < confusionMatrix[i].length; j++) {
+	    sb.append(String.format("%1$6s", "" + confusionMatrix[i][j]));
+	  }
+	  sb.append("\n");
+	}
+	System.out.println("Confusion Matrix");
+	System.out.println("-----------------");
+	System.out.println(sb.toString());
+
       } // end try (session)
-      
+
     } // end (env, opts)
-    
+
     System.out.println("Done!");
 
+  }
+
+  /**
+   * Find the maximum score and return its index.
+   *
+   * @param scores The scores.
+   * @return The index of the max.
+   */
+  public static int pred(float[] scores) {
+    float maxVal = Float.NEGATIVE_INFINITY;
+    int idx = 0;
+    for (int i = 0; i < scores.length; i++) {
+      if (scores[i] > maxVal) {
+	maxVal = scores[i];
+	idx = i;
+      }
+    }
+    return idx;
   }
 
   private static float[][] loadH5(String filename) {
@@ -157,12 +220,12 @@ public class ClassifyEvents {
       if (dataset_id >= 0) {
 	space_id = H5.H5Dget_space(dataset_id);
 	int ndims = H5.H5Sget_simple_extent_ndims(space_id);
-	System.out.println("No. of dimensions: " + ndims);
+//	System.out.println("No. of dimensions: " + ndims);
 	long[] dims = new long[ndims];
 	long[] maxdims = new long[ndims];
 	H5.H5Sget_simple_extent_dims(space_id, dims, maxdims);
-	System.out.println("Dimensions: " + Arrays.toString(dims));
-	System.out.println("Max. Dimensions: " + Arrays.toString(maxdims));
+//	System.out.println("Dimensions: " + Arrays.toString(dims));
+//	System.out.println("Max. Dimensions: " + Arrays.toString(maxdims));
 	dset_data = new float[(int) dims[0]][(int) dims[1]];
       }
 
@@ -171,7 +234,7 @@ public class ClassifyEvents {
 	attr_id = H5.H5Aopen_by_name(dataset_id, ".", "ActualTracesInFrame", HDF5Constants.H5P_DEFAULT, HDF5Constants.H5P_DEFAULT);
 	long[] ntraces = new long[1];
 	H5.H5Aread(attr_id, HDF5Constants.H5T_NATIVE_LONG, ntraces);
-	System.out.println("Traces in file: " + ntraces[0]);
+//	System.out.println("Traces in file: " + ntraces[0]);
 	if (attr_id >= 0) {
 	  H5.H5Aclose(attr_id);
 	}
